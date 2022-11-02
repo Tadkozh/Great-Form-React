4866490c22f530aefa3a2e468a43e977d5e48432
Tadkozh <tad.kozh.bihan@free.fr>
2022-11-02 19:51:11 +0100

    From vanilla to React : className, htmlFor, states

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Choose your form :
 
 + Contact form on a static html page
-https://github.com/Tadkozh/Great-Form
+https://github.com/Tadkozh/Great-Form (other repository)
 + Form on React without a library to retrieve form field states
 https://github.com/Tadkozh/Great-Form-React
 + Contact form with React-hook-form and Yup
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,64 +1,85 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Contact.css'
 
 const Contact = () => {
+
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [object, setObject] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const values = {
+    name, 
+    email, 
+    phone,
+    object, 
+    message,
+  };
+  console.log(values);
+
   return (
     <>
-      <h1 class = 'h1-contact'>Formulaire de contact</h1>
+      <h1 className='h1-contact'>Formulaire de contact</h1>
 
-      <form class='contact-form'>
-        <div class='form-content'>
+      <form className='contact-form'>
+        <div className='form-content'>
           
-          <label for='name' class='label-contact'>Nom et prénom : </label>
+          <label htmlFor='name' className='label-contact'>Nom et prénom : </label>
           <input 
-            class='input-contact' 
+            className='input-contact' 
             type='text' 
             id='name' 
             name='name' 
             placeholder='Nom et prénom' 
+            onChange={(e) => setName(e.target.value)}
           />
           
-          <label for='email' class='label-contact'>Adresse mail : </label>
+          <label htmlFor='email' className='label-contact'>Adresse mail : </label>
           <input 
-            class='input-contact' 
+            className='input-contact' 
             type='email' 
             id='email' 
             name='email' 
             placeholder='machin@truc.com'
+            onChange={(e) => setEmail(e.target.value)}
           />
           
-          <label for='phone' class='label-contact'>N° téléphone : </label>
+          <label htmlFor='phone' className='label-contact'>N° téléphone : </label>
           <input 
-            class='input-contact' 
+            className='input-contact' 
             type='text' 
             id='phone' 
             name='phone' 
-            placeholder='01234567' 
+            placeholder='01234567'
+            onChange={(e) => setPhone(e.target.value)}
           />
 
-          <label for='Sujet' class='label-contact'>Sujet : </label>
-          <select class='select-contact' name='subject'>
+          <label htmlFor='Sujet' className='label-contact'>Sujet : </label>
+          <select className='select-contact' name='subject' 
+            onChange={(e) => setObject(e.target.value)}>
             <option selected>Sélectionner l'objet de la demande</option>
             <option value='devis'>Devis</option>
             <option value='question'>Question</option>
             <option value='autre'>Autre</option>
           </select>
 
-          <label for='message' class='label-contact'>Message : </label>
+          <label htmlFor='message' className='label-contact'>Message : </label>
           <textarea 
-            class='message-contact'
+            className='message-contact'
             name='message'
             id='message'
             cols='20'
             rows='10'
             placeholder='Merci de poser vos question ou donner vos commentaire'
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
 
-          <label class='label-contact'>
+          <label className='label-contact'>
             <input type="checkbox" />En cochant cette case, j'accepte de recevoir des informations sur les différentes offres disponibles.
           </label>
 
-          <button class='button-contact' type='submit' value='Envoyer'>Envoyer</button>
+          <button className='button-contact' type='submit' value='Envoyer'>Envoyer</button>
 
         </div>
       </form>
